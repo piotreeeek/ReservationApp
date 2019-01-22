bc4bc7bb5bf260c4b06e692e2a453bef82aae24f
Piotrek <piotrek120017@gmail.com>
2019-01-22 20:26:07 +0100

    app initialize and migration for entities

---
+++ database/migrations/2019_01_22_184543_create_accessories_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccessoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accessories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('type_id');
+            $table->unsignedInteger('workplace_id')->nullable();
+            $table->string('model');
+            $table->string('mark')->unique();
+            $table->year('purchase_year');
+            $table->decimal('value', 6, 2);
+            $table->text('description');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('type_id')
+                ->references('id')->on('types');
+            $table->foreign('workplace_id')
+                ->references('id')->on('workplaces')
+                ->onDelete('set null')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accessories');
+    }
+}
---
+++ database/migrations/2019_01_22_185745_create_reservations_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('workplace_id');
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('workplace_id')->references('id')->on('workplaces')->onDelete('cascade');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
